3095c3f8631f987af0ee641427e5c5d34329030b
Alexei Bratuhin <alexei@openvalue.de>
2023-10-11 17:02:55 +0200

    make lombok pojos jackson-compatible

--- src/main/java/org/acme/Key.java
+++ src/main/java/org/acme/Key.java
@@ -1,8 +1,12 @@
 package org.acme;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
+@Builder
 @Data
+@Jacksonized
 public class Key {
     private final String key;
 }
--- src/main/java/org/acme/Value.java
+++ src/main/java/org/acme/Value.java
@@ -1,8 +1,12 @@
 package org.acme;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
+@Builder
 @Data
+@Jacksonized
 public class Value {
     private final String value;
 }
